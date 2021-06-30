56c55a9f93e3251f22571cf5405a8f4f461090fa
Colin Bassett <ccwbassett@gmail.com>
2021-06-29 17:16:02 -0700

    basic html/react scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Controls from './components/Controls';
+import UsersList from './components/UsersList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Github/Firestore User List</h1>
       </header>
+      <main>
+
+        <section>
+          <Controls />
+          
+          Notifications
+        </section>
+
+        <section>
+          <h3>Matching Users:</h3>
+          <UsersList />
+        </section>
+
+
+      </main>
     </div>
   );
 }
---
+++ src/components/Controls.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Controls() {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default Controls;
